271085f13b3268564ff434a89aa57a45e45fe124
untra <sam.volin@stackhawk.com>
2019-10-06 14:36:51 -0600

    tweak the launch scripts

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -7,8 +7,13 @@ echo "PRODUCTION MODE"
 
 rake assets:precompile
 
-./solr_start.sh -p
+echo "skipping legacy solr_start (but will we index?)"
+# ./scripts/solr_start.sh -p
+
+echo "skipping total_reload for now (but will we data?)"
+./scripts/total_reload.sh
 
 echo "Starting server."
-sudo passenger start -a 0.0.0.0 -p 80 -e production -d
+rails server -b localhost -p 3000 -e production -d
+# sudo passenger start -a 0.0.0.0 -p 80 -e production -d
 echo "Server daemon started. "
--- scripts/total_reload.sh
+++ scripts/total_reload.sh
@@ -1,40 +1,55 @@
 #!/bin/bash
+set -ex
 
 #kill solr process
-pkill -f solr
+# pkill -f solr
 
 #remove old data
-rm -rf solr/data
-rm -rf solr/default
-rm -rf solr/development
-rm -rf solr/pids
-rm -rf solr/test
+# rm -rf solr/data
+# rm -rf solr/default
+# rm -rf solr/development
+# rm -rf solr/pids
+# rm -rf solr/test
 
-while getopts 'pdt' flag; do
-  case "${flag}" in
-    d) export RAILS_ENV=development ;;
-    p) export RAILS_ENV=production ;;
-    t) export RAILS_ENV=test ;;
-    *) error "Unexpected option ${flag}" ;;
-  esac
-done
+# while getopts 'pdt' flag; do
+#   case "${flag}" in
+#     d) export RAILS_ENV=development ;;
+#     p) export RAILS_ENV=production ;;
+#     t) export RAILS_ENV=test ;;
+#     *) error "Unexpected option ${flag}" ;;
+#   esac
+# done
 
 #startup solr
-bundle exec rake sunspot:solr:start
+# bundle exec rake sunspot:solr:start
+export RAILS_ENV=production
+echo "💎 About to reload data for environment $RAILS_ENV"
 
 #all of our rake tasks
+# echo "🍒 rake rake sunspot:solr:start"
+# bundle exec rake sunspot:solr:start
+echo "🍒 rake rake db:create"
 bundle exec rake db:create
+echo "🍒 rake rake db:reset"
 bundle exec rake db:reset
+echo "🍒 rake rake db:migrate"
 bundle exec rake db:migrate
+echo "🍒 rake rake departments"
 bundle exec rake departments
+echo "🍒 rake rake import"
 bundle exec rake import
+echo "🍒 rake rake course_titles"
 bundle exec rake course_titles
+echo "🍒 rake rake ic_relations"
 bundle exec rake ic_relations
+echo "🍒 rake rake grades"
 bundle exec rake grades
 # build the hstore for departments
-
+echo "🍒 rake instructor_build_hstore"
 bundle exec rake instructor_build_hstore
+echo "🍒 rake course_build_hstore"
 bundle exec rake course_build_hstore
+echo "🍒 rake department_build_hstore"
 bundle exec rake department_build_hstore
 
 # remove unnecesarry departments
